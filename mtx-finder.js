c49cda1af7790abcc613a9aa2f30fc8e7fbff146
shuangliang kolu <shuangkolu@hotmail.com>
2016-04-17 19:22:23 +0300

    Recursively goes through all stash in league.

--- mtx-finder.js
+++ mtx-finder.js
@@ -13,11 +13,13 @@ stash
 
 var accountName = 'account';
 var selectedLeague = 'Perandus';
-var totalTabs = 0;
 
-
-setTimeout( stash_httpGetAsync( accountName, 1, selectedLeague, stash_initCallback ), 1000 );
-
+// Start stash loop
+// parameters are ( account name, stash tab index, league, callback )
+// account name and league are from user
+// stash tab index starts from 0
+// stash_callback is function that searches current stash and http request next stash.
+stash_httpGetAsync( accountName, 0, selectedLeague, stash_callback );
 
 function stash_httpGetAsync( account, tab, league, callback ){
 	
@@ -26,7 +28,7 @@ function stash_httpGetAsync( account, tab, league, callback ){
 	request.onreadystatechange = function(){
 		
 		if( request.readyState == 4 && request.status == 200 ){
-			callback( request.responseText );
+			callback( request.responseText, tab );
 		}
 		
 	}
@@ -36,17 +38,18 @@ function stash_httpGetAsync( account, tab, league, callback ){
 	request.send( null );
 }
 
-function stash_initCallback( data ){
+function stash_callback( data, currentTab ){
 	
 	var data_json = JSON.parse( data );
 	
-	totalTabs = data_json.numTabs
-	
 	console.log( 'selected league have ' + data_json.numTabs + ' tabs.' );
-}
-
-function stash_callback( data ){
+	console.log( 'this is tab nro: ' + currentTab + '.' );
 	
-	console.log( data );
+	// DO SOME DATA THINGS
 	
+	// Current stash tab is finished.
+	// Search next if there's any more.
+	if( currentTab < Number( data_json.numTabs ) ){
+		setTimeout( function(){ stash_httpGetAsync( accountName, Number( currentTab ) + 1, selectedLeague, stash_callback ) }, 1000 );
+	}
 }
